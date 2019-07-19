1747c16136aa1c37e17887e791fa40e2aff2187a
Eric Walker <walkereric10@gmail.com>
2019-07-18 20:38:36 -0400

    Initial Commit

---
+++ UserDefaultsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsDemo
+//
+//  Created by Eric Walker on 7/18/19.
+//  Copyright © 2019 Eric Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
